ce83bd175afb0d038774b8343687eb87cb5fe648
trevortrusty <trevortrusty@outlook.com>
2021-03-02 15:30:41 -0500

    create ctj header and definition file

---
+++ headers/ctj.cpp
@@ -0,0 +1,11 @@
+#include <ctj.h>
+
+int CTJ::setSourceFile(std::string filePath)
+{
+    return 1;
+}
+
+int CTJ::exportJSON()
+{
+    return 1;
+}
---
+++ headers/ctj.h
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <string>
+
+
+class CTJ
+{
+    private:
+        std::fstream internal_file;
+    public:
+        //===================================+=//
+        //  Input: Takes string of file path
+        //  Returns: If error, returns 0. Returns
+        //      1 on success
+        //===================================+=//
+        int setSourceFile(std::string); // sets the fstream object for the user's desired csv file
+
+        //===================================+=//
+        //  Returns: If error, returns 0. Returns
+        //      1 on success
+        //===================================+=//
+        int exportJSON();               // converts the source csv file to json and saves new file as "json_export.json"
+};
